d2ae6e2893d84fcb3f4f1a02c5fe92453b157235
Vikshith H G <127406967+vikshith-hg-c@users.noreply.github.com>
2024-07-15 12:52:22 +0530

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -11,13 +11,13 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            Building with modern technologies, Powred by LLM and Generative AI.
+            
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
         className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
-            Never misses a task, deadline or idea. Passinate about building Open-Source community.
+          
         </motion.div>
     </div>
   )
